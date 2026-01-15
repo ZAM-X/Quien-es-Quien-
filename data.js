c79f58022b3bdfbabb0d592ddb3b1b86f36ab4c6
ZAM-X <157859918+ZAM-X@users.noreply.github.com>
2026-01-15 18:22:18 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,199 @@
+/* data.js */
+const gameData = {
+    "videojuegos_juegos": {
+        title: "🎮 Videojuegos: Títulos",
+        items: [
+            "The Last of Us", "GTA V", "Minecraft", "Fortnite",
+            "RDR 2", "God of War", "Cyberpunk 2077", "Elden Ring",
+            "Zelda BOTW", "Resident Evil 4", "Silent Hill", "Halo",
+            "Call of Duty", "Among Us", "Undertale", "Cuphead",
+            "League of Legends", "Valorant", "The Sims", "Skyrim",
+            // Nuevos agregados (10)
+            "Overwatch", "Street Fighter", "Super Smash Bros", "FIFA",
+            "Metroid Dread", "Hollow Knight", "Dark Souls", "Bloodborne",
+            "Bioshock", "Tetris"
+        ]
+    },
+    "videojuegos_personajes": {
+        title: "🎮 Videojuegos: Personajes",
+        items: [
+            "Mario", "Kratos", "Master Chief", "Link",
+            "Joel", "Lara Croft", "Geralt de Rivia", "Arthur Morgan",
+            "Ezio Auditore", "Solid Snake", "Cloud Strife", "Pikachu",
+            "Sonic", "Samus Aran", "Aloy", "Nathan Drake",
+            "Sephiroth", "Jill Valentine", "Spyro", "Doomslayer",
+            // Nuevos agregados (10)
+            "Luigi", "Bowser", "Princess Peach", "Crash Bandicoot",
+            "Kirby", "Mega Man", "Dante", "Bayonetta",
+            "Ellie Williams", "Leon S. Kennedy"
+        ]
+    },
+    "peliculas": {
+        title: "🎬 Películas",
+        items: [
+            "El Padrino", "Caballero de la Noche", "Pulp Fiction", "Señor de los Anillos",
+            "Matrix", "Titanic", "Avatar", "Parque Jurásico",
+            "Volver al Futuro", "Forrest Gump", "Club de la Pelea", "Lista de Schindler",
+            "Gladiador", "Interestelar", "El Origen", "Bastardos sin Gloria",
+            "Toy Story", "El Resplandor", "Parásitos", "Buscando a Nemo",
+            // Nuevos agregados (10)
+            "Star Wars", "Harry Potter", "Los Vengadores", "El Rey León",
+            "Psicosis", "Tiburón", "La La Land", "Coco",
+            "Oppenheimer", "Spider-Man: Spider-Verse"
+        ]
+    },
+    "marcas": {
+        title: "🏷️ Marcas y Apps",
+        items: [
+            "Instagram", "TikTok", "YouTube", "Netflix",
+            "Spotify", "Disney", "PlayStation", "Xbox",
+            "Apple", "Samsung", "Nike", "Adidas",
+            "McDonald’s", "Burger King", "Coca-Cola", "Amazon",
+            "Tesla", "Google", "Nintendo", "Microsoft",
+            // Nuevos agregados (Solicitados + 1 para par)
+            "Zara", "Chanel", "HP", "Nvidia",
+            "Fly Emirates", "Qatar Airways", "Gemini", "Chat GPT",
+            "Pinterest", "Louis Vuitton"
+        ]
+    },
+    "lugares": {
+        title: "🌍 Lugares Icónicos",
+        items: [
+            "Torre Eiffel", "Estatua de la Libertad", "Coliseo Romano", "Gran Muralla China",
+            "Machu Picchu", "Taj Mahal", "Big Ben", "Sagrada Familia",
+            "Cristo Redentor", "Petra", "Ópera de Sídney", "Monte Fuji",
+            "Pirámides de Giza", "Chichén Itzá", "Burj Khalifa", "Stonehenge",
+            "Gran Cañón", "Catedral de San Basilio", "Torre de Pisa", "Monte Everest",
+            // Nuevos agregados (10)
+            "Cataratas del Niágara", "Acrópolis de Atenas", "Isla de Pascua", "Alhambra",
+            "Cataratas de Iguazú", "Parque Yellowstone", "Monte Rushmore", "Desierto del Sahara",
+            "Plaza Roja", "Antártida"
+        ]
+    },
+    "libros": {
+        title: "📚 Libros",
+        items: [
+            "Don Quijote", "Cien años de soledad", "El principito", "La Biblia",
+            "Amor en tiempos del cólera", "Hansel y Gretel", "Crónica muerte anunciada", "Señor de los Anillos",
+            "Harry Potter (Piedra)", "Diario de Ana Frank", "La Odisea", "Drácula",
+            "Frankenstein", "Juegos del hambre", "Romeo y Julieta", "1984",
+            "El Código Da Vinci", "Orgullo y Prejuicio", "El Alquimista", "Rayuela",
+            // Nuevos agregados (10)
+            "Moby Dick", "El Gran Gatsby", "Matar a un ruiseñor", "Los Miserables",
+            "La Divina Comedia", "Guerra y Paz", "Crimen y Castigo", "La Metamorfosis",
+            "El Hobbit", "Sherlock Holmes"
+        ]
+    },
+    "anime": {
+        title: "⛩️ Anime",
+        items: [
+            "Goku", "Naruto", "Luffy", "Saitama",
+            "Tanjiro", "Eren Yeager", "Edward Elric", "Spike Spiegel",
+            "Ichigo", "Shinji Ikari", "Ash Ketchum", "Deku",
+            "Sailor Moon", "Gohan", "Levi Ackerman", "Light Yagami",
+            "Killua", "Zoro", "Kakashi", "Inuyasha",
+            // Nuevos agregados (10)
+            "Vegeta", "Sasuke", "Natsu Dragneel", "All Might",
+            "Satoru Gojo", "Nezuko", "Mikasa Ackerman", "Asta",
+            "Yuji Itadori", "Gon Freecss"
+        ]
+    },
+    "superheroes": {
+        title: "🦸 Superhéroes",
+        items: [
+            "Spider-Man", "Batman", "Iron Man", "Superman",
+            "Wonder Woman", "Thor", "Hulk", "Capitán América",
+            "Wolverine", "Flash", "Deadpool", "Black Widow",
+            "Doctor Strange", "Black Panther", "Linterna Verde", "Aquaman",
+            "Daredevil", "Cíclope", "Joker", "Robin",
+            // Nuevos agregados (10)
+            "Ant-Man", "Scarlet Witch", "Visión", "Hawkeye",
+            "Green Arrow", "Shazam", "Star-Lord", "Rocket Raccoon",
+            "Groot", "Silver Surfer"
+        ]
+    },
+    "animales": {
+        title: "🐘 Animales",
+        items: [
+            "León", "Elefante", "Tigre", "Jirafa",
+            "Delfín", "Pingüino", "Lobo", "Oso Panda",
+            "Canguro", "Águila", "Tiburón", "Caballo",
+            "Gato", "Perro", "Gorila", "Cebra",
+            "Rinoceronte", "Orca", "Pulpo", "Búho",
+            // Nuevos agregados (10)
+            "Koala", "Perezoso", "Camello", "Zorro",
+            "Hipopótamo", "Cocodrilo", "Ballena Azul", "Loro",
+            "Colibrí", "Mariposa"
+        ]
+    },
+    "comida": {
+        title: "🍕 Comida y Platos",
+        items: [
+            "Pizza", "Hamburguesa", "Sushi", "Tacos",
+            "Pasta", "Paella", "Ramen", "Hot Dog",
+            "Lasagna", "Ensalada", "Filete", "Helado",
+            "Papas Fritas", "Burrito", "Pollo Frito", "Ceviche",
+            "Croissant", "Donas", "Waffles", "Sandwich",
+            // Nuevos agregados (10)
+            "Tiramisú", "Empanadas", "Curry", "Dumplings",
+            "Falafel", "Kebab", "Arepa", "Brownie",
+            "Cheesecake", "Mochi"
+        ]
+    },
+    "deportes": {
+        title: "⚽ Deportes",
+        items: [
+            "Fútbol", "Baloncesto", "Tenis", "Béisbol",
+            "Voleibol", "Golf", "Boxeo", "Fórmula 1",
+            "Natación", "Rugby", "Atletismo", "Ciclismo",
+            "Hockey", "Gimnasia", "Judo", "Surf",
+            "Skateboarding", "Ping Pong", "Bádminton", "Esquí",
+            // Nuevos agregados (10)
+            "Cricket", "Balonmano", "Polo", "Tiro con Arco",
+            "Esgrima", "Lucha Libre", "MMA", "Motociclismo",
+            "Remo", "Escalada"
+        ]
+    },
+    "paises": {
+        title: "🗺️ Países",
+        items: [
+            "España", "México", "Japón", "Estados Unidos",
+            "Brasil", "Francia", "Italia", "Alemania",
+            "Argentina", "China", "Reino Unido", "Canadá",
+            "Australia", "Colombia", "Rusia", "India",
+            "Egipto", "Sudáfrica", "Corea del Sur", "Turquía",
+            // Nuevos agregados (10)
+            "Grecia", "Portugal", "Tailandia", "Vietnam",
+            "Perú", "Chile", "Marruecos", "Suecia",
+            "Noruega", "Suiza"
+        ]
+    },
+    "musica": {
+        title: "🎵 Músicos y Bandas",
+        items: [
+            "Michael Jackson", "Freddie Mercury", "Shakira", "The Beatles",
+            "Bad Bunny", "Taylor Swift", "Elvis Presley", "Eminem",
+            "Madonna", "BTS", "Dua Lipa", "Bob Marley",
+            "Beyoncé", "Kurt Cobain", "Adele", "Bruno Mars",
+            "Rolling Stones", "Justin Bieber", "Rihanna", "Daft Punk",
+            // Nuevos agregados (10)
+            "Ed Sheeran", "Ariana Grande", "The Weeknd", "Drake",
+            "Metallica", "AC/DC", "Linkin Park", "Coldplay",
+            "Karol G", "Harry Styles"
+        ]
+    },
+    "caricaturas": {
+        title: "📺 Caricaturas",
+        items: [
+            "Homero Simpson", "Bob Esponja", "Mickey Mouse", "Bugs Bunny",
+            "Scooby Doo", "Tom y Jerry", "Minnie Mouse", "Pato Donald",
+            "Shrek", "Winnie Pooh", "Pinky y Cerebro", "Garfield",
+            "Las Chicas Superpoderosas", "Ben 10", "Phineas y Ferb", "Rick Sanchez",
+            "La Pantera Rosa", "Popeye", "Los Picapiedra", "Dora la Exploradora",
+            // Nuevos agregados (10)
+            "Dexter", "Coraje el Perro Cobarde", "Johnny Bravo", "Timmy Turner",
+            "Cosmo y Wanda", "Tío Rico", "Pato Lucas", "Silvestre",
+            "Piolín", "Kim Possible"
+        ]
+    }
+};
